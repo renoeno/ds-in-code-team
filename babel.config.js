ea08c64f1518a15d2ff1005848e5534b04b0be8b
renoeno <reno.less@gmail.com>
2022-07-17 15:15:39 -0300

    First commit

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = ["@babel/preset-env", "@babel/preset-react"];
+  const plugins = ["macros", "babel-plugin-transform-scss"];
+
+  return { presets, plugins };
+};
